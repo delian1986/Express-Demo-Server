b3d33f6313076c7a2d0edfe28ab94527ac87cc00
delian1986 <delian1914@gmail.com>
2019-02-04 09:53:47 +0200

    express user middleware corrected

--- config/express.js
+++ config/express.js
@@ -23,7 +23,7 @@ module.exports = app => {
 
     app.use((req, res, next) => {
         if (req.user) {
-            res.locals.currentUser = req.user;
+            res.locals.user = req.user;
         }
         next();
     });
